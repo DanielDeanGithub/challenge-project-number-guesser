bdda10ac621474dd8850f55baf4c9a266b144132
DanDean191 <github-new@inboxrefresh.com>
2024-02-17 17:43:32 +0000

    Created compareGuesses function to compare guesses and see which player is closer

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ let currentRoundNumber = 1;
 
 // Returns a random integer between 0 and 9
 const generateTarget = () => Math.floor(Math.random() * 10);
+
+// Compares guesses to see which player is closer
+const compareGuesses = (humanGuess, computerGuess, secretTarget) => {
+    const humanResult = Math.abs(secretTarget - humanGuess);
+    const computerResult = Math.abs(secretTarget - computerGuess);
+    return humanResult <= computerResult ? true : false;
+  };
